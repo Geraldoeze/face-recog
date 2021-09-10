45dde41f42a278060b7b8a833f41a2b031cadff5
Geraldii <egerald344@gmail.com>
2021-09-10 23:26:30 +0100

    added Logo folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
-import Navigation from './Comps/Navigation/Navigation'
+import Navigation from './Comps/Navigation/Navigation';
+import Logo from './Comps/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Logo />
     </div>
   );
 }
