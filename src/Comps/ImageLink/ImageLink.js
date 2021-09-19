350bf25d1ba45364287ccab841110772d7a0a220
Geraldoeze <egerald344@gmail.com>
2021-09-19 01:45:03 +0100

    Update Image

--- src/Comps/ImageLink/ImageLink.js
+++ src/Comps/ImageLink/ImageLink.js
@@ -4,6 +4,8 @@ const ImageLink = () => {
         <p className="f3">
             {'This Magic box will detect faces in your picture'}
              </p>
+            <div className="center">
+                </div> 
             <input className="f4 pa2  w-70 center" type="tex" />
             <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
         </div>
