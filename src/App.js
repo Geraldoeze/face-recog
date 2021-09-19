8766645e4c68d6358189f042cc43130280435962
Geraldoeze <egerald344@gmail.com>
2021-09-19 01:40:55 +0100

    fix errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './App.css';
-
+import Navigation from './Comps/Navigation/Navigation';
+import Logo from './Comps/Logo/Logo';
+import ImageLink from './Comps/ImageLink/ImageLink';
+import FaceRecognition from './Comps/Face-rec/Face-recognition'
 
 
 function App() {
   return (
-    <div className="App">
-      <h1> Learning React.js</h1>
+    <div>
+      <Navigation />
+      <Logo />
+      <ImageLink />
+      <FaceRecognition/>
     </div>
   );
 }
--- src/Comps/ImageLink/ImageLink.js
+++ src/Comps/ImageLink/ImageLink.js
@@ -5,7 +5,7 @@ const ImageLink = () => {
             {'This Magic box will detect faces in your picture'}
              </p>
             <input className="f4 pa2  w-70 center" type="tex" />
-            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Enter</button>
+            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
         </div>
     );
 }
