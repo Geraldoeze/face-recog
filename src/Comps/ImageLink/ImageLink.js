f39103fb9230edd85ee67a4c8bc4a79e9f4b24c0
Geraldoeze <egerald344@gmail.com>
2021-09-19 01:17:43 +0100

    styled app

--- src/Comps/ImageLink/ImageLink.js
+++ src/Comps/ImageLink/ImageLink.js
@@ -1,9 +1,11 @@
 const ImageLink = () => {
     return (  
         <div>
-            <p> Add a picture URL to get the face identified in the picture</p>
-            <input />
-            <button>Enter</button>
+        <p className="f3">
+            {'This Magic box will detect faces in your picture'}
+             </p>
+            <input className="f4 pa2  w-70 center" type="tex" />
+            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Enter</button>
         </div>
     );
 }
