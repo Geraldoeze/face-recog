358576714aafc06419ec16e4d6309e6a0045ea01
Geraldoeze <egerald344@gmail.com>
2021-09-22 12:57:21 +0100

    Update

--- src/Comps/ImageLink/ImageLink.js
+++ src/Comps/ImageLink/ImageLink.js
@@ -1,19 +1,21 @@
+import '../../App.css';
+import './Image link.css';
+
 const ImageLink = () => {
-    return (  
-        <div>
-        <p className="f3">
-            {'This Magic box will detect faces in your picture'}
-             </p>
-            <div className="center">
-                <div className="pa4 br3">
-                    
-                <input className="f4 pa2  w-70 center" type="tex" />
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
-    
-                </div>
-           </div>  
-       </div>
-    );
-}
- 
+ return (
+     <div>
+         <p className='f3 center'>
+             {'This magic brain will detect faces in your picture'}
+         </p>
+         <div className='center'>
+             <div className='center'>
+               <input className='f4 pa2 w-70 center' type='text'/>
+                  <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button> 
+              </div>
+         </div>
+     </div>
+ )
+
+};
+
 export default ImageLink;
--- src/Comps/Logo/Logo.js
+++ src/Comps/Logo/Logo.js
@@ -1,12 +1,12 @@
+import './Logo.css'
 import Tilt from 'react-tilt'
 
 const Logo = () => {
     return (
         <div  className='ma4 mt0'>
-        <Tilt className="Tilt br2 shadow-2 " options={{ max : 25 }} style={{ height: 120, width: 100 }} >
-        <div className="Tilt-inner pa3"> Brain image </div>
-        </Tilt>    
-            
+          <Tilt className="Tilt br2 shadow-2 " options={{ max : 25 }} style={{ height: 120, width: 100 }} >
+            <div className="Tilt-inner pa3"> Brain image </div>
+          </Tilt>    
         </div>
       );
 }
