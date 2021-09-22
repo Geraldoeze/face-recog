d68ab5b352d6129f56ed5a6672dc5753dc3d6d50
Geraldoeze <egerald344@gmail.com>
2021-09-22 09:06:40 +0100

    styles

--- src/Comps/Logo/Logo.js
+++ src/Comps/Logo/Logo.js
@@ -4,7 +4,7 @@ const Logo = () => {
     return (
         <div  className='ma4 mt0'>
         <Tilt className="Tilt br2 shadow-2 " options={{ max : 25 }} style={{ height: 120, width: 100 }} >
-        <div className="Tilt-inner pa3"> 👽 </div>
+        <div className="Tilt-inner pa3"> Brain image </div>
         </Tilt>    
             
         </div>
